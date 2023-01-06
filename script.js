9c03f28d2628b3681d2a2b17fafadad64b4f7895
ComicMatthew <mateusz.a.staniek@gmail.com>
2023-01-06 16:40:48 +0100

    nothing to comment

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const radiusInput = document.getElementById("radius");
 const latInput = document.getElementById("lat");
 const longInput = document.getElementById("long");
 
-//Function that should GET the restaurant values --> push into a JSON and put it into the HTML document
+//Function that should GET the restaurant values --> push into a JSON and put it into the HTML
 
 function showRestaurant() {
 
